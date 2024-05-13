f5c1668b37d1ff85fb9392cfed68da9d13e2d608
GurunathKokate <gurunathkokati@gmail.com>
2024-05-13 11:49:10 +0530

    initial commit

---
+++ hello.js
@@ -0,0 +1,10 @@
+// File name: hello.js
+
+// Define a function to print "Hello, World!"
+function sayHello() {
+    console.log("Hello, World!");
+  }
+  
+  // Call the function to print the message
+  sayHello();
+  
